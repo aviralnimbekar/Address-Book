a232875c47ac03e826c74ac0db6cc6e43f7683f0
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-09-05 18:30:20 +0530

    ADDED : Ability to add person to address book

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 /**
  * Purpose - To develop an address book system using collection lib
  */
@@ -7,5 +9,9 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome to address book problem statement");
+
+        Scanner scanner = new Scanner(System.in);
+        ContactCreation contactCreation = new ContactCreation();
+        contactCreation.addContact(scanner);
     }
 }
